d26f19813aa24dfd8b7ad0aa2d7b9557ae0a7f62
Highdrien <adrien.zabban@outlook.fr>
2023-04-10 17:09:18 +0200

    change path

--- submisson/parameter.py
+++ submisson/parameter.py
@@ -1,11 +1,11 @@
 import os
 
-PATH = '..'         # 'gdrive/MyDrive/Compétition_IA'
-S1_PATH = os.path.join(PATH, 'assignment-2023/s1')
-S2_PATH = os.path.join(PATH, 'assignment-2023/s2')
-S2_01_PATH = os.path.join(PATH, 'assignment-2023/s2_01')
-MASK_PATH = os.path.join(PATH, 'assignment-2023/s2-mask')
-MASK_01_PATH = os.path.join(PATH, 'assignment-2023/s2-mask_01')
+PATH = os.path.join('..', 'new_assignment-2023')
+S1_PATH = os.path.join(PATH, 's1')
+S2_PATH = os.path.join(PATH, 's2')
+S2_01_PATH = os.path.join(PATH, 's2_01')
+MASK_PATH = os.path.join(PATH, 's2-mask')
+MASK_01_PATH = os.path.join(PATH, 's2-mask_01')
 TRAIN_SPLIT = 0.8
 VAL_SPLIT = 0.1
 TEST_SPLIT = 0.1

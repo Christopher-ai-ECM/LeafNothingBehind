f484a17224a467852d960a64fb94b37624dcef17
Highdrien <adrien.zabban@outlook.fr>
2023-04-10 15:49:29 +0200

    first submission

---
+++ submisson/main.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+def main(options):
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Options
+    parser.add_argument('--mode', default='infer', type=str, 
+                        help="this argument will be given the value 'infer'")
+
+    parser.add_argument('--csv_path', type=str, 
+                        help="we will use this argument to give your code the path to the CSV file for the test \
+                            data. Those CSV files will obey the same format as the train dataset, and the folder\
+                            structure of the dataset will also be the same")
+    
+    parser.add_argument('--save_infers_under', type=str, 
+                        help="we will use this argument to give your code the path to the folder where to save \
+                            the results.")
+
+    args = parser.parse_args()
+    options = vars(args)
+
+    main(options)

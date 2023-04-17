a062eab0aa9a94a5abc61a45a4cfb424b9cb1175
Highdrien <adrien.zabban@outlook.fr>
2023-04-17 19:19:55 +0200

    add test the result.pickel

--- test_submission/check_result.py
+++ test_submission/check_result.py
@@ -1,5 +1,29 @@
 import pickle
-import numpy
+import numpy as np
+import torch
+import tqdm
+import os
+
+from skimage import io
+from torch import nn
+
+
+class MSE(nn.Module):
+    def __init__(self, cloud_index):
+        super(MSE, self).__init__()
+        self.cloud_index = cloud_index
+
+    def forward(self, S2_pred, S2_true, S2_mask):
+        mask = torch.ones_like(S2_mask, dtype=torch.bool)
+        for index in self.cloud_index:
+            mask &= (S2_mask != index)
+        
+        S2_true_masked = S2_true[mask]
+        S2_pred_masked = S2_pred.squeeze(1)[mask]
+        
+        loss = torch.mean(torch.pow((S2_true_masked - S2_pred_masked), 2))
+        
+        return loss
 
 
 def read_pickle(result_path):
@@ -28,7 +52,7 @@ def check_shapes(result):
     bool = True
     try:
 
-        if type(outputs) != numpy.ndarray:
+        if type(outputs) != np.ndarray:
             print('erreur, ce n est pas un numpy array')
             bool = False
 
@@ -62,21 +86,44 @@ def check_shapes(result):
     return bool
 
 
-def test_result(result_path):
+def load_image(path):
+    image = io.imread(path)
+    if len(image.shape) == 3:
+        image = np.moveaxis(image, -1, 0)
+    return torch.from_numpy(image)
+
+
+def load_tiff(names, path):
+    n = len(names)
+    data = torch.empty((n, 256, 256))
+    for i, name in enumerate(names):
+        data[i] = load_image(os.path.join(path, name))
+    return data
+
+
+def test_result(result_path, data_path):
     result = read_pickle(result_path)
     if not(check_attributes(result)) or not(check_shapes(result)) :
         exit('erreur, le test ne va pas se lancer')
+    else:
+        print('resultat dans le bon format \n')
+    
     outputs = result['outputs']
-    paths = result['paths']
+    names = result['paths']
+    outputs = torch.from_numpy(outputs.squeeze(axis=-1))
 
-    
-    
+    s2_path = os.path.join(data_path, 's2')
+    mask_path = os.path.join(data_path, 's2-mask')
+    s2 = load_tiff(names, s2_path)
+    mask = load_tiff(names, mask_path)
 
+    clouds_index = [8, 9]
+    loss = MSE(clouds_index)
+    loss_value = loss(outputs, s2, mask)
+    print('test loss:', loss_value)
     
 
 if __name__ == "__main__":
-    result_path = 'result\\test_data\\results.pickle'
-    result = read_pickle(result_path)
-    check_attributes(result)
-    bool = check_shapes(result)
-    print('erreur dans le code:', not(bool))
+    result_path = 'result\\test_csv\\results.pickle'
+    data_path = '..\\data'
+    test_result(result_path, data_path)
